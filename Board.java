833013cb54a0ce339141561a0a114c79b593d4a3
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-11-05 20:28:33 -0500

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,11 +1,38 @@
-//Board file
-//Whats in the box
-//5 ships
-//Carrier has 5 pegs 
 
-//2 boards
-	//rows and columns 
-	//10x10
-	//letters down the side
-	//numbers across top 
-//2 colored sets of pegs
+public class Board
+{
+
+    public static void main (String[] args) 
+     {
+	    
+	    char[][] board = new char [10][10];
+	  
+	    board[9][9] = 'X';
+	    
+	 
+	    toString(board);
+	 
+      }
+//there are two parameters are row and col which check to see if there is a ship
+    public boolean checkShip(int row, int col) {
+	
+	   if (board[row][col] == 'X') {
+          return true;
+        } else {
+           return false;
+		}
+   	}
+
+//The method is toString which takes a 2D array fillBoard as a parameter 
+//This will print the rows and columns of the board 10 x10
+    public void toString (char fillBoard[][]){
+	   for(int r = 0; r < 10; r++){
+		 for(int c = 0; c < 10; c++){
+			System.out.print(fillBoard[r][c] + " ");
+		  }
+	    System.out.println();
+	   }
+     }
+}
+
+
