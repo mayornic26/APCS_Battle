00dd9fdf2833454df0f17d5d7a0886396d7b6d29
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-11-06 09:28:06 -0500

    Update Tester.java
    in class

--- Tester.java
+++ Tester.java
@@ -1,4 +1,13 @@
-//Main Driver or Tester
-//test1
-//test
-//tester
+
+public class Tester { 
+	
+	public static void main(String[] args){
+
+		Board player1 = newBoard()
+		
+		player1.placeShip([1][1]) = 's'
+		
+		player1.printShips();
+		
+	}
+}
