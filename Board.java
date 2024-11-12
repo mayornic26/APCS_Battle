c6e73834b27f68c8e89da266cae3b7182937864b
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-11-12 08:53:15 -0500

    Update Board.java
    second one

--- Board.java
+++ Board.java
@@ -1,22 +1,15 @@
 
-public class Board
-{
-
-    public static void main (String[] args) 
-     {
-	    
-	    char[][] board = new char [10][10];
-	  
-	    board[9][9] = 'X';
-	    
-	 
-	    toString(board);
-	 
-      }
+public class Board {
+	
+	char[][] ships;
+	
+	public Board(){
+		ships = new char[10][10];
+}
       
  //method to place the ships at shiprow(sr) and shipcol(s)
       public void placeShip (int sr, int sc) {
-		  board[r][c] = 's';
+		  board[sr][sc] = 's';
 	  }
 	  
 	  public boolean checkShip (char[][] withShips int r, int c){
@@ -27,27 +20,7 @@ public class Board
 				 return false;
 			}
 		}
-		
-	  
-//there are two parameters are row and col which check to see if there is a ship
-    public boolean checkShip(int row, int col) {
-	
-	   if (board[row][col] == 'X') {
-          return true;
-        } else {
-           return false;
-		}
-   	}
 
-	public void printShips(){
-		for(int r - 0; r < 10; r++){
-			for(int c = 0; c <10; c++){
-				System.out.print(" " + ships[r][c]);
-			}//close the column loop
-			System.out.print("\n");
-		}
-	}
-			
 //The method is toString which takes a 2D array fillBoard as a parameter 
 //This will print the rows and columns of the board 10 x10
     public void toString (char fillBoard[][]){
