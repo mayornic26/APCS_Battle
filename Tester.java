87f91235986e350c0106f1a9a35c08e6ce59747d
mayornic26 <114428885+mayornic26@users.noreply.github.com>
2024-11-12 09:15:09 -0500

    Update Tester.java
    the right one

--- Tester.java
+++ Tester.java
@@ -3,9 +3,9 @@ public class Tester {
 	
 	public static void main(String[] args){
 
-		Board player1 = newBoard()
+		Board player1 = new Board();
 		
-		player1.placeShip([1][1]) = 's'
+		player1.placeShip(1,1);
 		
 		player1.printShips();
 		
